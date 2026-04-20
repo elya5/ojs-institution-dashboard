4b31614b0b256ac6e889868d808f6a4e8dadcb74
Elias Entrup <elias.entrup@tib.eu>
2026-04-20 09:52:08 -0700

    Add formatting

--- dashboard/data_loader.py
+++ dashboard/data_loader.py
@@ -14,9 +14,9 @@ from pyalex import Works
 
 load_dotenv()
 logger = logging.getLogger(__name__)
-DATAVERSE_API_KEY = os.getenv("DATAVERSE_API_KEY")
-DATAVERSE_URL = "https://dataverse.harvard.edu/api/access/dataset/:persistentId/?persistentId=doi:10.7910/DVN/OCZNVY"
-pyalex.config.api_key = os.getenv("OPENALEX_API_KEY")
+DATAVERSE_API_KEY = os.getenv('DATAVERSE_API_KEY')
+DATAVERSE_URL = 'https://dataverse.harvard.edu/api/access/dataset/:persistentId/?persistentId=doi:10.7910/DVN/OCZNVY'
+pyalex.config.api_key = os.getenv('OPENALEX_API_KEY')
 
 
 @click.command()
@@ -25,16 +25,16 @@ def download_beacon_dataset() -> Path:
     logger.info('Downloading beacon.csv')
     response = requests.get(
         DATAVERSE_URL,
-        headers={"X-Dataverse-key": DATAVERSE_API_KEY},
+        headers={'X-Dataverse-key': DATAVERSE_API_KEY},
         stream=True,
-        timeout=60
+        timeout=60,
     )
 
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir_path = Path(tmpdir)
-        zip_path = tmpdir_path / "dataset.zip"
+        zip_path = tmpdir_path / 'dataset.zip'
         zip_path.write_bytes(response.content)
-        with zipfile.ZipFile(zip_path, "r") as z:
+        with zipfile.ZipFile(zip_path, 'r') as z:
             z.extractall(tmpdir_path)
 
         shutil.move(tmpdir_path / 'beacon.csv', Path('beacon.csv'))
@@ -51,7 +51,7 @@ def download_openalex_journal_articles(issns: list[str]) -> list[dict]:
 def download_ror_dataset() -> Path:
     """Download ROR csv dataset to ror_data.csv."""
     logger.info('Starting to download ROR entities.')
-    r = requests.get("https://zenodo.org/api/records/18985120", timeout=10).json()
+    r = requests.get('https://zenodo.org/api/records/18985120', timeout=10).json()
     r = requests.get(r['links']['files'], timeout=10).json()
     r = requests.get(r['entries'][0]['links']['content'], timeout=60)
     logger.info('Download complete. Starting to unpack and update database.')
